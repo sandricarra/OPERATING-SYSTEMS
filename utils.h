b2a29bb86032c5ba722f7dec256aa9dfdae4be76
sandricarra <105009788+sandricarra@users.noreply.github.com>
2024-12-11 21:16:02 +0100

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,78 +1,87 @@
 
-#ifndef TP_SO_24_25_H
-#define TP_SO_24_25_H
-
+#ifndef UTILS_H
+#define UTILS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <signal.h>
 #include <errno.h>
 #include <pthread.h>
 #include <sys/select.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-
+// Constantes
 #define MAN_PIPE "manager_pipe"
 #define FEED_PIPE "user_pipe_%d"
-#define MAX_CLIENTES 10
-#define MAX_TOPICOS 20
-#define MAX_MENSSAGENS 5
+#define MAX_USERS 10
+#define MAX_TOPICS 20
+#define MAX_MESSAGES 5
+#define MAX_MSG_SIZE 300
+#define MAX_NAME_LENGTH 20
+#define MAX_TOPIC_LENGTH 20
+#define MAX_FIFO_LENGTH 128
 
 
-typedef struct{
-  char menssagem[300];
-  int pid;
-  int duracao;
-  char nome[20];
-  char topico[20];
-  int fechado;
-  int bloqueado;
-} msgStruct;
 
-typedef struct{
-  char menssagem[300];
-  int pid;
-  int tempoVida;
-  char topico[20];
-  char nome[20];
-} msgPersStruct;
-
-
+// Estructura para representar un tema
 typedef struct {
-  char errorMenssagem[300];
-  int pid;
-} errorStruct;
+    char topicName[MAX_TOPIC_LENGTH];
+    int subscriberCount;
+    int state;  // 0->Desbloqueado 1->Bloqueado
+} Topic;
 
-typedef struct{
-  char topico[20];
-  char username[20];
-  int pid;
-  char FIFO[128];
-  int tipo;                               // 0 - Inscrever ; 1 - Desinscrever ; 2 - Validar nome ; 3 - Saida ; 4 - Amostrar topicos
-} pedidoStruct;
-
-
+// Estructura para un usuario
 typedef struct {
-  char nomeTopico[20];
-  int contInscritos;
-  int estado;                             // 1 - Bloqueado ; 0 - Desbloqueado
-} topico;
+    int pid;
+    char name[MAX_NAME_LENGTH];
+    int topicCount;
+    char FIFO[MAX_FIFO_LENGTH];
+    Topic subscribedTopics[MAX_USERS];
+} User;
 
+// Estructura para un mensaje estándar
 typedef struct {
-  int pid;
-  char nome[20];
-  int numTopicos;
-  char FIFO[128];
-  topico topicosIns[MAX_CLIENTES];
-} cliente;
+    char message[MAX_MSG_SIZE];
+    int pid;
+    int duration;
+    char name[MAX_NAME_LENGTH];
+    char topic[MAX_TOPIC_LENGTH];
+    int closed;
+    int blocked;
+} Message;
 
+// Estructura para un mensaje persistente
 typedef struct {
-  pthread_mutex_t *m;
-  int stop;
-} TDADOS;
+    char message[MAX_MSG_SIZE];
+    int pid;
+    int lifetime;
+    char topic[MAX_TOPIC_LENGTH];
+    char name[MAX_NAME_LENGTH];
+} PersistMsg;
 
-#endif // !TP_SO_24_25_H
+// Estructura para solicitudes de cliente
+typedef struct {
+    char topic[MAX_TOPIC_LENGTH];
+    char username[MAX_NAME_LENGTH];
+    int pid;
+    char FIFO[MAX_FIFO_LENGTH];
+    int type;  // 0->Subscribirse 1->Desubscribirse 2->Registro Usuario 3->Eliminación usuario
+} Request;
+
+// Estructura para mensajes de error
+typedef struct {
+    char errorMessage[MAX_MSG_SIZE];
+    int pid;
+} Error;
+
+// Estructura de datos para sincronización
+typedef struct {
+    pthread_mutex_t *mutex;
+    int stop;
+} TDATA;
+
+#endif // UTILS_H
